d4446e902c64b0c1274dd6d30cccb0c8c0a9d02f
abc <shamailabbas61@gmail.com>
2021-02-24 23:15:13 +0500

    fourth commit

--- src/components/Otherprojects.js
+++ src/components/Otherprojects.js
@@ -28,49 +28,42 @@ const Otherprojects = () => {
             </p>
           </div>
         </a>
-        <a
-        // href='https://beachresortbooking.netlify.app/'
-        // rel='noreferrer'
-        // target='_blank'
-        >
-          <div className='griditems'>
-            <h3>Gmail Clone using Firebase and React Redux</h3>
-            <p>
-              A fully functional Gmail Clone with following features <br />
-              <li>User Authantication</li>
-              <li>Send Emails</li>
-              <li>Receive Emails</li>
-              <li>Persistant Login</li>
-              <li>Database Integration</li>
-              <h3>Tech used to bulid this project</h3>
-              <li>Firebase</li>
-              <li>Reactjs</li>
-              <li>React redux</li>
-              <li>Nodejs</li>
-            </p>
-          </div>
-        </a>
-        <a
-        //  href='' rel='noreferrer' target='_blank'
-        >
-          <div className='griditems'>
-            <h3>Tinder Clone using React Context Api</h3>
-            <p>
-              A fully functional Tinder Clone having following features
-              <br />
-              <li>User Authantication</li>
-              <li>UserDashboard</li>
-              <li>Swipe Functionality</li>
-              <li>Chat</li>
-              <li>Database Integration</li>
-              <h3>Tech used to bulid this project</h3>
-              <li>Firebase</li>
-              <li>Reactjs</li>
-              <li>React redux</li>
-              <li>Nodejs</li>
-            </p>
-          </div>
-        </a>
+
+        <div className='griditems'>
+          <h3>Gmail Clone using Firebase and React Redux</h3>
+          <p>
+            A fully functional Gmail Clone with following features <br />
+            <li>User Authantication</li>
+            <li>Send Emails</li>
+            <li>Receive Emails</li>
+            <li>Persistant Login</li>
+            <li>Database Integration</li>
+            <h3>Tech used to bulid this project</h3>
+            <li>Firebase</li>
+            <li>Reactjs</li>
+            <li>React redux</li>
+            <li>Nodejs</li>
+          </p>
+        </div>
+
+        <div className='griditems'>
+          <h3>Tinder Clone using React Context Api</h3>
+          <p>
+            A fully functional Tinder Clone having following features
+            <br />
+            <li>User Authantication</li>
+            <li>UserDashboard</li>
+            <li>Swipe Functionality</li>
+            <li>Chat</li>
+            <li>Database Integration</li>
+            <h3>Tech used to bulid this project</h3>
+            <li>Firebase</li>
+            <li>Reactjs</li>
+            <li>React redux</li>
+            <li>Nodejs</li>
+          </p>
+        </div>
+
         <a
           href='https://amazonclonebyshamail.netlify.app/'
           rel='noreferrer'
@@ -94,52 +87,42 @@ const Otherprojects = () => {
             </p>
           </div>
         </a>
-        <a
-        // href='https://beachresortbooking.netlify.app/'
-        // rel='noreferrer'
-        // target='_blank'
-        >
-          <div className='griditems'>
-            <h3>Zoom Clone using React React-Redux</h3>
-            <p>
-              A fully functional Zoom Clone having following features
-              <br />
-              <li>User Authantication</li>
-              <li>UserDashboard</li>
-              <li>AdminDashboard</li>
-              <li>Chat Rooms</li>
-              <li>Database Integration</li>
-              <h3>Tech used to bulid this project</h3>
-              <li>Firebase</li>
-              <li>Reactjs</li>
-              <li>React redux</li>
-              <li>Nodejs</li>
-            </p>
-          </div>
-        </a>
-        <a
-        // href='https://beachresortbooking.netlify.app/'
-        // rel='noreferrer'
-        // target='_blank'
-        >
-          <div className='griditems'>
-            <h3>Pinterest Clone using React Redux</h3>
-            <p>
-              A fully functional Pinterest having following features
-              <br />
-              <li>User Authantication</li>
-              <li>UserDashboard</li>
-              <li>AdminDashboard</li>
-              <li>Sorting</li>
-              <li>Database Integration</li>
-              <h3>Tech used to bulid this project</h3>
-              <li>Firebase</li>
-              <li>Reactjs</li>
-              <li>React redux</li>
-              <li>Nodejs</li>
-            </p>
-          </div>
-        </a>
+
+        <div className='griditems'>
+          <h3>Zoom Clone using React React-Redux</h3>
+          <p>
+            A fully functional Zoom Clone having following features
+            <br />
+            <li>User Authantication</li>
+            <li>UserDashboard</li>
+            <li>AdminDashboard</li>
+            <li>Chat Rooms</li>
+            <li>Database Integration</li>
+            <h3>Tech used to bulid this project</h3>
+            <li>Firebase</li>
+            <li>Reactjs</li>
+            <li>React redux</li>
+            <li>Nodejs</li>
+          </p>
+        </div>
+
+        <div className='griditems'>
+          <h3>Pinterest Clone using React Redux</h3>
+          <p>
+            A fully functional Pinterest having following features
+            <br />
+            <li>User Authantication</li>
+            <li>UserDashboard</li>
+            <li>AdminDashboard</li>
+            <li>Sorting</li>
+            <li>Database Integration</li>
+            <h3>Tech used to bulid this project</h3>
+            <li>Firebase</li>
+            <li>Reactjs</li>
+            <li>React redux</li>
+            <li>Nodejs</li>
+          </p>
+        </div>
       </div>
     </div>
   )
