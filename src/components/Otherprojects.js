b9c9c484ea070d2c2452f6b062838815251be57a
unknown <Shamail>
2021-05-25 11:41:55 +0500

    image changed

--- src/components/Otherprojects.js
+++ src/components/Otherprojects.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import '../css/others.css'
-import { a } from 'react-router-dom'
+import React from "react";
+import "../css/others.css";
+import { a } from "react-router-dom";
 const Otherprojects = () => {
   return (
-    <div className='archivesection'>
-      <div className='worthyprojects'>
+    <div className="archivesection">
+      <div className="worthyprojects">
         <a
-          href='https://beachresortbooking.netlify.app/'
-          rel='noreferrer'
-          target='_blank'
+          href="https://beachresortbooking.netlify.app/"
+          rel="noreferrer"
+          target="_blank"
         >
-          <div className='griditems'>
+          <div className="griditems">
             <h3>Hotel Booking Web App using React Redux</h3>
             <p>
-              A fully functional Hotel Booking Webapp having following features{' '}
+              A fully functional Hotel Booking Webapp having following features{" "}
               <br />
               <li>User Authantication</li>
               <li>UserDashboard</li>
@@ -28,79 +28,89 @@ const Otherprojects = () => {
             </p>
           </div>
         </a>
- <a
-          href='https://airbnbclonebyshamail.netlify.app/'
-          rel='noreferrer'
-          target='_blank'
+        <a
+          href="https://airbnbclonebyshamail.netlify.app/"
+          rel="noreferrer"
+          target="_blank"
         >
-        <div className='griditems'>
-          <h3>.....AirBnb Clone.....</h3>
-    <br/>
-    <br/>
-    <br/>
-              <p>
-            A Frontend  Clone with following features <br />
-            <li>DatePicker</li>
-            <li> Search</li>
-            <li>Explore Nearby</li>
-          <br/>
-    <br/>
-           
-            <h3>Tech used to bulid this project</h3>
-            <li>Material Ui</li>
-            <li>Reactjs</li>
-            <li>React redux</li>
-            
-          </p>
-        </div>
-</a>
-     <a
-          href='https://gmailclonebyshamail.netlify.app/'
-          rel='noreferrer'
-          target='_blank'
-        >
-      <div className='griditems'>
-          <h3>Gmail Clone using Firebase and React Redux</h3>
-          <p>
-            A fully functional Gmail Clone with following features <br />
-            <li>User Authantication</li>
-            <li>Send Emails</li>
-            <li>Receive Emails</li>
-            <li>Persistant Login</li>
-            <li>Database Integration</li>
-            <h3>Tech used to bulid this project</h3>
-            <li>Firebase</li>
-            <li>Reactjs</li>
-            <li>React redux</li>
-            <li>Nodejs</li>
-          </p>
-        </div>
-    </a>
-    
-        <div className='griditems'>
-          <h3>Youtube Clone with Complete Backend support</h3>
-          <p>
-            A fully functional Youtube Clone having following features
+          <div className="griditems">
+            <h3>.....AirBnb Clone.....</h3>
             <br />
-         <br/>  
-   
-    
-          <h2> Deploy soon...</h2>
-    <br/>
-            <h3>Tech used to bulid this project</h3>
-            <li>Mongodb</li>
-            <li>Reactjs</li>
-            <li>React redux</li>
-            <li>Nodejs</li>
-          </p>
-        </div>
+            <br />
+            <br />
+            <p>
+              A Frontend Clone with following features <br />
+              <li>DatePicker</li>
+              <li> Search</li>
+              <li>Explore Nearby</li>
+              <br />
+              <br />
+              <h3>Tech used to bulid this project</h3>
+              <li>Material Ui</li>
+              <li>Reactjs</li>
+              <li>React redux</li>
+            </p>
+          </div>
+        </a>
+        <a
+          href="https://gmailclonebyshamail.netlify.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="griditems">
+            <h3>Gmail Clone using Firebase and React Redux</h3>
+            <p>
+              A fully functional Gmail Clone with following features <br />
+              <li>User Authantication</li>
+              <li>Send Emails</li>
+              <li>Receive Emails</li>
+              <li>Persistant Login</li>
+              <li>Database Integration</li>
+              <h3>Tech used to bulid this project</h3>
+              <li>Firebase</li>
+              <li>Reactjs</li>
+              <li>React redux</li>
+              <li>Nodejs</li>
+            </p>
+          </div>
+        </a>
+        <a
+          href="https://fiverr-clone.vercel.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="griditems">
+            <h3>Fiverr Clone with Complete Backend support</h3>
+            <p>
+              A fully functional Fiverr Clone having following features
+              <br />
+              <br />
+              <li>User Authantication</li>
+              <li>Place Order</li>
+              <li>Create Gigs</li>
+              <li>Persistant Login</li>
+              <li>Database Integration</li>
+              <li>Payment Integration</li>
+              <li>Job Posting</li>
+              <li>Submit Proposal</li>
+              <li>Message Client</li>
+              <br />
+              <h3>Tech used to bulid this project</h3>
+              <li>MySQL</li>
+              <li>Reactjs</li>
+              <li>React redux</li>
+              <li>Nodejs</li>
+              <li>Graphql</li>
+            </p>
+          </div>
+        </a>
 
         <a
-          href='https://amazonclonebyshamail.netlify.app/'
-          rel='noreferrer'
-          target='_blank'
+          href="https://amazonclonebyshamail.netlify.app/"
+          rel="noreferrer"
+          target="_blank"
         >
-          <div className='griditems'>
+          <div className="griditems">
             <h3>Amazon Clone Web App using Reactjs</h3>
             <p>
               A fully functional E-commerce Webapp having following features
@@ -119,7 +129,7 @@ const Otherprojects = () => {
           </div>
         </a>
 
-        <div className='griditems'>
+        <div className="griditems">
           <h3>Zoom Clone using React React-Redux</h3>
           <p>
             A fully functional Zoom Clone having following features
@@ -136,11 +146,9 @@ const Otherprojects = () => {
             <li>Nodejs</li>
           </p>
         </div>
-
-        
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Otherprojects
+export default Otherprojects;
